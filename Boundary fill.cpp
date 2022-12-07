4ad40cf7688fd6e2da72077a13c0f97e3dc5580a
smit-V <119999492+smit-V@users.noreply.github.com>
2022-12-07 11:04:34 +0530

    Update Boundary fill.cpp

--- Boundary fill.cpp
+++ Boundary fill.cpp
@@ -1,25 +1,6 @@
 #include<graphics.h>
-#include<stdlib.h>
 #include<iostream>
-#include<conio.h>
-#include<dos.h>
-int main()
-{  
-	void boundary_fill(int x,int y,int f,int b);
-	/* request auto detection */
-	int gdriver = DETECT, gmode;
-	/* initialize graphics and local variables */
-	initgraph(&gdriver, &gmode, "C:\\TC\\BGI");
-	setcolor(getmaxcolor());
 
-	/* draw the circle */
-	circle(100, 100, 10);
-	boundary_fill(100,100,4,15);
-
-	/* clean up */
-	getch();
-	closegraph();
-}
 void boundary_fill(int x,int y,int f,int b)
 {
 	if(getpixel(x,y)!=b && getpixel(x,y)!=f)
@@ -31,4 +12,20 @@ void boundary_fill(int x,int y,int f,int b)
 		boundary_fill(x,y+1,f,b);
 		boundary_fill(x,y-1,f,b);
 	}
+}
+
+int main()
+{  
+	void boundary_fill(int x,int y,int f,int b);
+	
+	/* int gdriver = DETECT, gmode;
+	initgraph(&gdriver, &gmode, "C:\\TC\\BGI"); */
+	initwindow(800,800);
+	setcolor(getmaxcolor());
+	
+	circle(100, 100, 10);
+	boundary_fill(100,100,4,15);
+	
+	getch();
+	closegraph();
 }
